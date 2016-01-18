4d3e9b1e348a431b28a3e3cf4f9ccaa1e3a53aa2
Daniel Suo <danielsuo@gmail.com>
2016-01-18 11:40:51 -0500

    Add SiftMatch struct. Fixes #13

--- extras/matching.h
+++ extras/matching.h
@@ -9,6 +9,24 @@ typedef enum {
   MatchSiftDistanceL2
 } MatchSiftDistance;
 
+typedef struct {
+  uint id;            // image id for tracking
+  uint idx;           // SiftPoint index in SiftData.*_data
+  float xim_coord;    // x coordinate in image
+  float yim_coord;    // y cooordinate in image
+  float x3D_coord;    // x coordinate in space
+  float y3D_coord;    // y coordinate in space
+  float z3D_coord;    // z coordinate in space
+} SiftMatchPointDesc;
+
+typedef struct {
+  SiftMatchPointDesc point1;
+  SiftMatchPointDesc point2;
+  float score;
+  float ambiguity;
+  float error;
+} SiftMatch;
+
 // Perform an exhaustive search between all sift key points between two images
 double MatchSiftData(SiftData &data1, SiftData &data2, MatchSiftDistance distance = MatchSiftDistanceL2);
 
